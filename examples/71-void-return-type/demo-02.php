10fbd57fa9f6a2023ddbb32f97bd9a8271827b4b
Pascal MARTIN <contact@pascal-martin.fr>
2016-08-14 18:58:22 +0200

    Démos : void return type

---
+++ examples/71-void-return-type/demo-02.php
@@ -0,0 +1,18 @@
+<?php
+
+function returns_a_value()
+{
+    // Cette fonction retourne une valeur
+    return 42;
+}
+
+function returns_nothing()
+{
+    // Cette fonction retourne null
+    return;
+}
+
+function does_not_return()
+{
+    // Cette fonction retourne elle aussi null
+}
---
+++ examples/71-void-return-type/demo-03.php
@@ -0,0 +1,17 @@
+<?php
+
+function returns_nothing() : void
+{
+    // Cette fonction retourne null
+    return;
+}
+
+function does_not_return() : void
+{
+    // Cette fonction retourne elle aussi null
+}
+
+function returns_a_value() : void
+{
+    return 42;
+}
